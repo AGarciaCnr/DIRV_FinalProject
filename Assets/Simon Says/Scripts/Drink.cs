9b7a73188b7238d75a2ebbd37bd6811c2ce3c1f1
Alejandro Garcia Cancer <agarciacnr@gmail.com>
2024-02-21 21:58:53 +0100

    Added Liquid shader

---
+++ Assets/Simon Says/Scripts/Drink.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
+
+public class Drink : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] public float _fill;
+    private float _initialFill;
+    [SerializeField] private float _emptyingTime = 5;
+    [SerializeField] private AnimationCurve _dumpAngle;
+
+    private float _time = 0;
+    private Transform _transform;
+    private ParticleSystem.EmissionModule _emission;
+
+    private void Awake()
+    {
+        _emission = GetComponentInChildren<ParticleSystem>().emission;
+        _transform = this.transform;
+        _initialFill = _fill;
+    }
+
+    private void Update()
+    {
+        if (!IsEmpty() && IsDumped())
+        {
+            _emission.enabled = true;
+            Empty();
+
+            _fill = (_fill <= 0.25f) ? 0.0f : _fill;
+        }
+        else
+        {
+            _emission.enabled = false;
+        }
+    }
+
+    private void Empty()
+    {
+        _time += Time.deltaTime;
+        _fill = Mathf.Lerp(_initialFill, 0, _time / _emptyingTime);
+    }
+
+    private bool IsEmpty()
+    {
+        return _fill <= 0;
+    }
+
+    private bool IsDumped()
+    {
+        Vector3 drinkAngles = new Vector3(_transform.eulerAngles.x, 0.0f, _transform.eulerAngles.z);
+        Debug.Log(drinkAngles);
+        float currentAngle = Quaternion.Angle(Quaternion.Euler(drinkAngles), Quaternion.identity);
+        float dumpAngle = _dumpAngle.Evaluate(_fill);
+        return currentAngle > dumpAngle;
+    }
+}
