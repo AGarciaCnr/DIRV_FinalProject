5a0db08bf7e15a831604f0d897010d195af7f193
JaikuG <jaimefesd@gmail.com>
2024-03-03 17:33:02 +0100

    Catch-a-Robot Scene FInished

--- Assets/Space/Scripts/RobotMovement.cs
+++ Assets/Space/Scripts/RobotMovement.cs
@@ -5,20 +5,67 @@ using UnityEngine.AI;
 
 public class RobotMovement : MonoBehaviour
 {
-    GameObject m_robot;
-    public GameObject m_goal;
-    NavMeshAgent m_agent;
+    public GameObject m_astronaut;
+    public List<GameObject> m_waypoints;
 
-    // Start is called before the first frame update
+    private NavMeshAgent m_agent;
+
+    private int m_currentWaypointIndex = 0;
+    private bool m_fleeing = false;
+
     void Start()
     {
-        m_robot = this.gameObject;
         m_agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        m_agent.destination = m_goal.transform.position;   
+        // Comprobar si el objetivo está cerca
+        if (Vector3.Distance(transform.position, m_astronaut.transform.position) < 8f)
+        {
+            // Huir del objetivo
+            Vector3 directionToGoal = transform.position - m_astronaut.transform.position;
+            Vector3 fleePosition = transform.position + directionToGoal.normalized * 15f;
+            m_agent.destination = fleePosition;
+
+            m_fleeing = true;
+        }
+        else
+        {
+            if (m_fleeing)
+            {
+                // Buscar el punto más cercano
+                float closestDistance = Mathf.Infinity;
+                GameObject closestWaypoint = null;
+
+                foreach (GameObject waypoint in m_waypoints)
+                {
+                    float distance = Vector3.Distance(transform.position, waypoint.transform.position);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestWaypoint = waypoint;
+
+                        m_currentWaypointIndex = m_waypoints.IndexOf(waypoint);
+                    }
+                }
+
+                m_fleeing = false;
+            }
+
+            // Establecer el destino del agente al waypoint actual
+            m_agent.destination = m_waypoints[m_currentWaypointIndex].transform.position;
+
+            // Comprobar si el agente ha llegado al waypoint actual
+            if (Vector3.Distance(transform.position, m_waypoints[m_currentWaypointIndex].transform.position) < 2f)
+            {
+                // Pasar al siguiente waypoint
+                m_currentWaypointIndex = (m_currentWaypointIndex + 1) % m_waypoints.Count;
+            }
+        }
+
+
+
+
     }
 }
