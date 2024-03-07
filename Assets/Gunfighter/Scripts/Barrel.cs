b7ad21e4a196338541484865da3ebe90e4fdea49
Alejandro Garcia Cancer <agarciacnr@gmail.com>
2024-03-07 21:06:54 +0100

    Added GunfighterManager

--- Assets/Gunfighter/Scripts/Barrel.cs
+++ Assets/Gunfighter/Scripts/Barrel.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
+    [SerializeField] private GameObject _explosionPrefab;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent(out Rigidbody bulletRigidbody))
         {
             this.gameObject.SetActive(false);
+            Explode();
         }
+    }
+
+    private void Explode()
+    {
+        Instantiate(_explosionPrefab, transform.position, Quaternion.Euler(transform.rotation.eulerAngles.x + 90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
     }
 }
--- Assets/Gunfighter/Scripts/GunfighterManager.cs
+++ Assets/Gunfighter/Scripts/GunfighterManager.cs
@@ -1,18 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GunfighterManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _prefab; // El _prefab que deseas instanciar
+    [SerializeField] private int _numberOfInstances = 10; // El número de instancias que deseas crear
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void StartGame()
+    {
+
+        for (int i = 0; i < _numberOfInstances; i++)
+        {
+            Vector3 randomPosition = RandomNavmeshLocation(10); // Genera una posición aleatoria en el NavMesh
+            Instantiate(_prefab, new Vector3(randomPosition.x, randomPosition.y + 0.5f, randomPosition.z), Quaternion.Euler(transform.rotation.eulerAngles.x + 90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z)); // Instancia el _prefab en la posición generada
+
+        }
+    }
+
+    // Método para generar una posición aleatoria en el NavMesh
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 }
